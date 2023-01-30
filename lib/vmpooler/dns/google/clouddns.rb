1cd8af4fa145c803c704a043454e34ae2116b09a
Jake Spain <jake.spain+15749776@perforce.com>
2023-01-30 15:29:30 -0500

    Run bundle gem vmpooler-dns-google-clouddns

---
+++ lib/vmpooler/dns/google/clouddns.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative "clouddns/version"
+
+module Vmpooler
+  module Dns
+    module Google
+      module Clouddns
+        class Error < StandardError; end
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/vmpooler/dns/google/clouddns/version.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Vmpooler
+  module Dns
+    module Google
+      module Clouddns
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
